864b8aa43079d3226dee5ca4407c7693010099b2
Will Melbourne <melbourne@arrival.com>
2020-02-20 14:17:46 +0000

    svg clock

--- examples/Example08Time.elm
+++ examples/Example08Time.elm
@@ -4,6 +4,8 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import Svg exposing (Svg, line, svg)
+import Svg.Attributes exposing (height, stroke, viewBox, width, x1, x2, y1, y2)
 import Task
 import Time
 
@@ -88,23 +90,60 @@ subscriptions model =
 
 -- VIEW
 
+buttonMsg : Model -> String
+buttonMsg model =
+    case model.subscribe of
+        True ->
+            "stop Clock!"
+        False ->
+            "start Clock"
+
+getSecond: Model -> Float
+getSecond m = toFloat (Time.toSecond m.zone m.time)
+
+angle: Float -> Float
+angle s = (s / 60) * 360
+
+x: Float -> String
+x a = String.fromFloat(60 + (sin (degrees a))* 60)
+
+y: Float -> String
+y a = String.fromFloat (60 - (cos (degrees a)) * 60)
+
+secondHand: Float -> Svg Msg
+secondHand sec =
+    line [x1 "60", y1 "60", x2 (x (angle sec)), y2 (y (angle sec)), stroke "black"] []
+
+
 
 view : Model -> Html Msg
 view model =
   let
-    hour   = String.fromInt (Time.toHour   model.zone model.time)
-    minute = String.fromInt (Time.toMinute model.zone model.time)
-    second = String.fromInt (Time.toSecond model.zone model.time)
+    hourString   = String.fromInt (Time.toHour   model.zone model.time)
+    minuteString = String.fromInt (Time.toMinute model.zone model.time)
+    second = Time.toSecond model.zone model.time
+    secondString = String.fromInt second
+    testSecond = toFloat (Time.toSecond model.zone model.time)
   in
-  div []
+  div [style "font-family" "Arial", style "padding" "30px"]
     [
-        h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ],
+        h1 [] [ text (hourString ++ ":" ++ minuteString ++ ":" ++ secondString) ],
         p [] [ text (case model.subscribe of
                         True ->
-                            "True"
+                            "Clock is active"
                         False ->
-                            "FAlse"
+                            "Clock is inactive"
                      )],
         p [] [ text (String.fromInt model.counter)]
-        , button [ onClick IncreaseCounter, style "display" "block" ] [ text "stop Clock!" ]
+        , button [ onClick IncreaseCounter, style "display" "block" ] [ text (buttonMsg model) ]
+        , svg [ width "120"
+                  , height "120"
+                  , viewBox "0 0 120 120"
+                  , style "border" "solid black"
+                  ] [ secondHand (getSecond model) ],
+              --] [ secondHand 180 ],
+
+        p [] [ text ("angle:" ++ String.fromFloat (angle (toFloat second)))]
+        , p [] [ text ("x:" ++ (x (angle(toFloat second))))]
+        , p [] [ text ("y:" ++ (y (angle(toFloat second))))]
         ]
